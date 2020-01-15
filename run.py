d5556db3a3124deb845573d0e7a26135b5437c84
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-01-15 21:29:59 +0000

    Added chat text box function and rearranged code to make it shorter

--- run.py
+++ run.py
@@ -1,42 +1,50 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the `messages` list"""
-    now = datetime.now().strftime("%H: %M: %S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    """To send a message use: /USERNAME/MESSAGE"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for("user", username = session["username"]))
+
     return render_template("index.html")
-    
 
 
-@app.route("/<username>")
+@app.route("/chat/<username>", methods = ["GET", "POST"])
 def user(username):
-    """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    """Add and Display chat messages"""
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages( username, message)
+        return redirect(url_for("user", username = session["username"]))
+    return render_template("chat.html", username = username, chat_messages = messages)
+    """ We are using the return redirect to stop the message from being sent repeatedly """
+    """<h1>Welcome, {0}</h1>{1}".format(username, messages)"""
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
-    add_messages(username, message)
+    add_message(username, message)
     return redirect("/" + username)
 
 
-
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT", 8000)), debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
