b39f0e276a481cdc26f186a498cae3a4c4834292
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-01-17 10:24:20 +0000

    Added text box functionality and shortend run.py code

--- run.py
+++ run.py
@@ -33,18 +33,18 @@ def user(username):
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
-        add_messages( username, message)
+        add_message( username, message)
         return redirect(url_for("user", username = session["username"]))
     return render_template("chat.html", username = username, chat_messages = messages)
     """ We are using the return redirect to stop the message from being sent repeatedly """
     """<h1>Welcome, {0}</h1>{1}".format(username, messages)"""
 
 
-@app.route("/<username>/<message>")
+"""@app.route("/<username>/<message>")
 def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
+    '''Create a new message and redirect back to the chat page'''
     add_message(username, message)
-    return redirect("/" + username)
+    return redirect("/" + username)"""
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
