3d8946bfe6e21347b742d0ad62b9442e78cf0daa
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-01-17 10:37:48 +0000

    Added Procfile for deployment

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 
 app = Flask(__name__)
-app.secret_key = "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
 
@@ -47,4 +47,4 @@ def send_message(username, message):
     return redirect("/" + username)"""
 
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", "5000")), debug=False)
